b0cf3246f0b634b4c6f9073b33735b9396e8f106
Rock <winterhue21@gmail.com>
2025-11-05 23:45:24 +0530

    Update Knapsack_Frac.py

--- Knapsack_Frac.py
+++ Knapsack_Frac.py
@@ -1,33 +1,49 @@
-# ---------- FRACTIONAL KNAPSACK ----------
+values = [60,20,100,120]
+weights = [10,0,20,30]
 
-def fractional_knapsack(values, weights, capacity):
+capacity = 50
+
+def fractional_knapsack(values, weights, W):
+
     n = len(values)
-    # Compute value/weight ratio
-    ratio = [(values[i]/weights[i], weights[i], values[i]) for i in range(n)]
-    
-    # Sort items by ratio in descending order
-    ratio.sort(reverse=True)
-    
+    items = []
+    remaining = W
     total_value = 0.0
-    for r, w, v in ratio:
-        if capacity == 0:
-            break
-        if w <= capacity:
-            # Take full item
-            total_value += v
-            capacity -= w
+    fractions = [0.0]*n
+
+    for i in range(n):
+        if weights[i] == 0: # zero wieght item to first priority.
+            if values[i] > 0:
+                total_value += values[i]
+                fractions[i] = 1.0
         else:
-            # Take fraction of item
-            total_value += r * capacity
-            capacity = 0
-    return total_value
+            items.append((values[i]/weights[i], i, values[i], weights[i]))
 
-# ---------- MAIN ----------
+
+
+    items.sort(reverse=True)
+
+    for ratio,i,v,w in items:
+        if remaining<=0:
+            break
+        take = min(w,remaining)
+        frac = take/w if w > 0 else 0.0
+        fractions[i] = frac
+
+        total_value += frac*v
+        remaining -= take
+
+    return total_value, fractions
+
+
+max_val , fracs = fractional_knapsack(values, weights, capacity)
+
+print("Maximum value in knapsack:", max_val)
+print("Fractions of items taken:", fracs)
+
+----------------------------------------------------------------------
+# ---------- USER INPUT ----------
 n = int(input("Enter number of items: "))
-values = list(map(int, input("Enter values separated by space: ").split()))
-weights = list(map(int, input("Enter weights separated by space: ").split()))
+values = list(map(int, input(f"Enter {n} values separated by space: ").split()))
+weights = list(map(int, input(f"Enter {n} weights separated by space: ").split()))
 capacity = int(input("Enter knapsack capacity: "))
-
-max_value = fractional_knapsack(values, weights, capacity)
-print("Maximum value that can be taken in knapsack:", round(max_value, 2))
-
